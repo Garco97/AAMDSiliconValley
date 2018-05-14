0afc55f103c38496ff2fbdd975d52c493f1e1658
Garco <dgarciacosio@gmail.com>
2018-05-14 22:19:35 +0200

    Preparacion del trabajo

---
+++ Trabajo/eclipse-workspace/Conversor/src/conversor/Ejecucion.java
@@ -0,0 +1,10 @@
+package conversor;
+
+import java.io.IOException;
+
+public class Ejecucion {
+ public static void main (String[]args) throws IOException {
+	 Conversor conversor = new Conversor();
+	 conversor.procesamiento();
+ }
+}
